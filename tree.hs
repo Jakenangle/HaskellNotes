8e215db053e5a93d369c7498414b6e4a114acd97
Jacob Nangle <Jacob.Nangle@gmail.com>
2013-06-04 11:25:51 -0400

    .

--- tree.hs
+++ tree.hs
@@ -4,3 +4,13 @@ data Tree a = Empty|Node a (Tree a) (Tree a)
 depth::Tree a ->Integer
 depth Empty = 0
 depth (Node x l r) = 1 + max (depth l) (depth r)
+
+bstInsert::a->Tree b->Tree c
+bstInsert a Empty = (Node a Empty Empty)
+bstInsert a (Node b l r)
+	|a<b 		= (Node b (bstInsert a l) r)
+	|otherwise 	= (Node b l (bstInsert a r))
+
+flatten::Tree a -> [a]
+flatten Empty = []
+flatten (Node a l r) = flatten l ++ [a] ++ flatten r
